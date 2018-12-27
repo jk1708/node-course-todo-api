fc25645b3f3f697ba3965220dfb3dd4cea04add9
Jaishree Khatri <jaishreekhatri17@gmail.com>
2018-12-27 23:20:27 +0530

    testing for delete for /todo/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,3 +98,43 @@ describe('Get /todo/:id',()=>{
 
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todo/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+
+
+
+  });
+  it('should return 404 if todo not found', (done)=>{
+    var hexId= new ObjectID().toHexString();
+    request(app)
+     .delete(`/todo/${hexId}`)
+     .expect(404)
+     .end(done);
+  });
+  it('should return 404 for non-object ids', (done)=>{
+    request(app)
+    .delete('/todo/123abc')
+    .expect(404)
+    .end(done);
+
+  });
+});
